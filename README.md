c5a8f4c75d3d63a7d7925bdd523bb88615941907
Jumitti <112354223+Jumitti@users.noreply.github.com>
2023-12-16 01:27:52 +0100

    Release

---
+++ README.md
@@ -0,0 +1,140 @@
+![](img/img.png)
+## Introduction
+
+Anonymous Telebot is first and foremost a proof-of-concept python script. Where did I get this (possibly bad) idea? Telegram is very secure. Anonymity is the basis of this application. However, if you don't configure the application, other users can see your nametag (and even your phone number, etc.). And if they have your nametag, they can communicate with you. Let's face it, this is an extremely rare situation. But even if it is rare, I'm sure you'll have enough ideas to imagine all the things you can do with my Telegram bot. So what does it do?
+
+As soon as you enter this Telegram bot, you're asked for a password. The password is set by the bot owner. If you know the password, you can log in and experience total anonymity. You'll be given an atitled name (e.g. USER1). You can then send messages through the Telegram bot to other bot users without knowing their Telegram ID, nametag or anything else. You can also send photos and videos. Note that if USER1 sends a message to USER2, only these 2 users will see the messages, not the others. This bot allows you to have totally private and anonymous conversions. 
+
+Another point that seemed essential to me was not to retrieve any information. That's why users have "USER" IDs and no information or IDs are stored. Everything is saved as a variable, which means that if the script crashes, all users are lost. 
+
+Personally, I've used it to play a kind of "hide-and-seek" game. I sent the password to lots of friends and the aim was to find out who was which user. 
+
+## Features
+
+Common:
+- Send messages, photos, videos from one USER to another
+- Display list of USERS
+- Report a user
+
+For USERS:
+- Delete your USER at any time
+
+For OWNER:
+- Password-restricted access to the Telebot
+- Password can be changed via bot.
+- Ability to see users trying to log in, those who have logged in and those who have been banned (function only available for security reasons).
+- Possibility of banning a user
+
+## My Anonymous Telebot for test
+
+An example is worth 1,000 words: [Anonymous Telebot](https://t.me/keepthesecret_bot)
+
+1. Connect to [Anonymous Telebot](https://t.me/keepthesecret_bot):
+   - A welcome message is sent to you, asking for a password.
+2. Send the password: AnonymousTelebot@
+3. You have access to the chat ! :)
+
+## How to use (For USER)
+
+List of users:
+- ``/users``
+
+To send a message to someone (e.g. USER1):
+- ``/USER1 Hello World``
+
+To chat with the bot owner:
+- ``/USEROWNER Hello world``
+
+To display help in the bot:
+- ``/help``
+
+To delete your user:
+- ``/delete_account``
+
+To sign a user:
+- ``/REPORT``
+
+## How to use (For OWNER)
+
+The same functions are available as for USERS.
+
+If you want to create your own Anonymous Telebot, please refer to the "Create your own Anonymous Telebot" section. Here we'll look at the actions available to Telegram bot owners.
+
+If you have forgotten your password:
+- ``/password``
+
+To change the password (e.g. HELLOWORLD):
+- ``/setpassword HELLOWORLD``
+
+To ban a user (e.g. USER1):
+- ``/DEL /USER1``
+
+As OWNER and for security reasons, I allow the OWNER to see which users are trying to log in, which have used the correct password and which have been banned.
+
+List of users trying to connect: 
+- ``/waiting``
+
+List of users who have used the correct password:
+- ``/verified``
+
+List of banned users:
+- ``/banned``
+
+## Create your own Anonymous Telebot
+1. Install [Telepot](https://github.com/nickoala/telepot):
+    ```
+    pip install telepot
+    ```
+    (For Raspberry only) Install [gpiozero](https://gpiozero.readthedocs.io/en/latest/):
+    ```
+    pip install gpiozero
+    ```
+2. Extract ```anonymoustelebot.zip``` where you want → [Release](https://github.com/Jumitti/Anonymous_Telebot/releases/tag/v1.0)
+   - (For Raspberry) Don't forget to give all permissions at ```anonymoustelebot``` folder:
+     - In ```anonymoustelebot``` folder, open a terminal:
+     ```
+     sudo 777 anonymous_telebot.py
+     sudo 777 SECRETS.py
+     ```
+3. Config ```SECRETS.json``` in ```anonymoustelebot``` folder
+
+   - How to get your **ID_OWNER**:
+     - send ```/getid``` to [myidbot](https://telegram.me/myidbot) on [Telegram](https://web.telegram.org/k/)
+     - Copy/paste your ID in ```SECRETS.json``` without (') or (")
+   
+   - How to get your **TOKEN**:
+     - Config a bot with [@BotFather](https://telegram.me/BotFather):
+       - Create a bot with ```/newbot``` and follow instructions
+       - Get API token with ```/mybots```, select your bot and get API token
+       - Copy/paste your token in ```SECRETS.json``` between (') or (")
+     - Don't forget to send ```/start``` at your Telegram bot
+
+   - **ID** is the list of ID whitelisted
+
+   - **password** is the password to get access to the Telegrambot
+4. Run ```anonymous_telebot.py```:
+   - In ```anonymoustelebot``` folder, open a terminal:
+     ```
+     python3 anonymous_telebot.py
+     ```
+5. That all folks ! You can talk with you Anonymous Telebot 🎉
+
+*Note: If you stop the script, all USERS will disappear.*
+
+## DISCLAIMER: USE OF ANONYMOUS TELEGRAM BOT
+
+1. The user acknowledges and agrees that the use of the Telegram Bot is entirely at their own risk. The operator of the Telegram Bot does not guarantee the accuracy, relevance, completeness, or quality of the information provided.
+
+2. The operator of the Telegram Bot disclaims any responsibility for any harm, loss, or damage, whether direct, indirect, consequential, or otherwise, arising from the use or inability to use the Telegram Bot.
+
+3. The user is solely responsible for the use of the Telegram Bot and must comply with all applicable laws, rules, and regulations. The operator of the Telegram Bot assumes no liability for illegal or unauthorized use.
+
+4. The operator of the Telegram Bot reserves the right to modify, suspend, or discontinue the service at any time, without notice or liability.
+
+By using the Telegram Bot, the user acknowledges having read, understood, and accepted the terms of this disclaimer. Continued use of the Telegram Bot constitutes ongoing acceptance of the terms of this disclaimer.
+
+## Credit & Licence
+
+Copyright (c) 2023 [Minniti Julien](https://github.com/Jumitti).
+
+This software is distributed under an MIT licence. Please consult the LICENSE file for more details.
--- anonymous_telebot.py
+++ anonymous_telebot.py
@@ -1,3 +1,23 @@
+# Copyright (c) 2023 Minniti Julien
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of Anonymous Telebot and associated documentation files, to deal
+# in Anonymous Telebot without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of Anonymous Telebot, and to permit persons to whom Anonymous Telebot is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of Anonymous Telebot.
+
+# Anonymous Telebot IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH Anonymous Telebot OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 from datetime import datetime
 import time
 import telepot
@@ -7,6 +27,7 @@ import json
 import requests
 
 
+# Retrieve information of owner, whitelist and bot
 def secret():
     script_directory = os.path.dirname(os.path.abspath(__file__))
     secrets_path = os.path.join(script_directory, 'SECRETS.json')
@@ -23,24 +44,23 @@ def secret():
     return chat_id_key, chat_id_owner, password, token
 
 
+# Change password
 def change_password(new_password):
     script_directory = os.path.dirname(os.path.abspath(__file__))
     secrets_path = os.path.join(script_directory, 'SECRETS.json')
     with open(secrets_path, 'r') as secrets_file:
         secrets = json.load(secrets_file)
-
-    # Changer le mot de passe
     secrets['password'] = new_password
-
-    # Écrire les modifications dans le fichier JSON
     with open(secrets_path, 'w') as secrets_file:
         json.dump(secrets, secrets_file)
 
 
+# Initializing of Question To Answer
 def qta_set(chat_id, answer):
     qta[chat_id] = {'answer': answer}
 
 
+# Help
 def help(chat_id):
     help_text = " /help - For help obviously\n/users - List of users\n/delete_account - To delete your account here\n/REPORT - To report someone. /USEROWNER will contact you.\n\n" \
                 "How to send message to someone (e.g. USER1):\n/USER1 Hello World\n\n" \
@@ -49,6 +69,7 @@ def help(chat_id):
     bot.sendMessage(chat_id, help_text, parse_mode='HTML')
 
 
+# Display user list
 def users(chat_id):
     users_text = "/USEROWNER"
     for i in inverse_chat_id_verified:
@@ -57,9 +78,11 @@ def users(chat_id):
     bot.sendMessage(chat_id, users_text, parse_mode='HTML')
 
 
+# Main
 def handle(msg):
-    content_type, _, chat_id = telepot.glance(msg)
+    content_type, _, chat_id = telepot.glance(msg)  # Retrieve information about message
 
+    # Is a Question to Answer ?
     try:
         answer = qta[chat_id]['answer']
     except KeyError:
@@ -88,22 +111,22 @@ def handle(msg):
     if chat_id in chat_id_verified.values() or chat_id == chat_id_owner:
 
         # Utils
-        if command == "/help":
+        if command == "/help":  # Send /help
             help(chat_id)
 
-        elif command == "/users":
+        elif command == "/users":  # Send users list
             users(chat_id)
 
-        elif command == '/delete_account' and chat_id != chat_id_owner:
+        elif command == '/delete_account' and chat_id != chat_id_owner:  # Delete your user
             bot.sendMessage(chat_id, f"See you soon {inverse_chat_id_verified[chat_id]}")
             del chat_id_verified[inverse_chat_id_verified[chat_id]]
 
-        elif command == '/REPORT':
+        elif command == '/REPORT':  # Report a user  (question)
             bot.sendMessage(chat_id, f'Who wanted to report you?')
             answer = 1
             qta_set(chat_id, answer)
 
-        elif answer == 1:
+        elif answer == 1:  # Report a user (answer)
             del qta[chat_id]
             bot.sendMessage(chat_id,
                             f"/USEROWNER is alerted about your report about {command}. You will be contacted soon by /USEROWNER")
@@ -111,15 +134,17 @@ def handle(msg):
 
         # Message from VERIFIED
         elif command.startswith("/USER") and chat_id != chat_id_owner:
-            if command.startswith("/USEROWNER"):
+
+            if command.startswith("/USEROWNER"):  # Send messages to OWNER
                 if content_type == 'photo':
                     bot.sendPhoto(chat_id_owner, photo_id, f"From {inverse_chat_id_verified[chat_id]} to {command}")
                 elif content_type == 'video':
                     bot.sendVideo(chat_id_owner, video_id, caption=f"From {inverse_chat_id_verified[chat_id]} to {command}")
                 else:
                     bot.sendMessage(chat_id_owner, f"From {inverse_chat_id_verified[chat_id]} to {command}")
+
             else:
-                for id_verified in chat_id_verified:
+                for id_verified in chat_id_verified:  # Send messages to USER
                     if command.startswith(id_verified):
                         if content_type == 'photo':
                             bot.sendPhoto(chat_id_verified[id_verified], photo_id,
@@ -133,11 +158,16 @@ def handle(msg):
 
         # Message from OWNER
         elif chat_id == chat_id_owner:
-            if command == "/waiting":
-                bot.sendMessage(chat_id_owner, chat_id_waiting, parse_mode='HTML')
-            elif command == "/verified":
-                bot.sendMessage(chat_id_owner, chat_id_verified, parse_mode='HTML')
-            elif command.startswith("/USER"):
+            if command == "/waiting":  # Send list of waiting ID
+                bot.sendMessage(chat_id_owner, chat_id_waiting)
+
+            elif command == "/verified":  # Send list of verified ID
+                bot.sendMessage(chat_id_owner, chat_id_verified)
+
+            elif command == "/banned":  # Send list of banned ID
+                bot.sendMessage(chat_id_owner, chat_id_banned)
+
+            elif command.startswith("/USER"):  # Send messages to USER by OWNER
                 for id_verified in chat_id_verified:
                     if command.startswith(id_verified):
                         if content_type == 'photo':
@@ -149,15 +179,18 @@ def handle(msg):
                         else:
                             bot.sendMessage(chat_id_verified[id_verified],
                                             f"From /USEROWNER to {command}")
-            elif command.startswith("/setpassword "):
+
+            elif command.startswith("/setpassword "):  # Change password
                 new_password = command.split("/setpassword ")[1]
                 change_password(new_password)
                 _, _, password, _ = secret()
                 bot.sendMessage(chat_id_owner, f"Password changed to {password}")
-            elif command == "/password":
+
+            elif command == "/password":  # Send password to OWNER
                 _, _, password, _ = secret()
                 bot.sendMessage(chat_id_owner, f"Password: {password}")
-            elif command.startswith("/DEL "):
+
+            elif command.startswith("/DEL "):  # Ban a user
                 user = command.split("/DEL ")[1]
                 if user in inverse_chat_id_verified.values():
                     bot.sendMessage(chat_id_verified[user], "Access removed. You are banned.")
@@ -167,14 +200,16 @@ def handle(msg):
 
     # Connection to the Anonymous Bot
     elif chat_id not in chat_id_key:
-        if chat_id not in chat_id_waiting and chat_id not in chat_id_verified.values() and chat_id not in chat_id_banned:
+        if chat_id not in chat_id_waiting and chat_id not in chat_id_verified.values() and chat_id not in chat_id_banned:  # For new users
             chat_id_waiting.append(chat_id)
             message_to_unknown_id = f'Welcome to Anonymous TelegramBot, please enter password.'
             bot.sendMessage(chat_id, message_to_unknown_id)
-        elif chat_id in chat_id_banned:
+
+        elif chat_id in chat_id_banned:  # Block banned users
             bot.sendMessage(chat_id, f"Access removed. You are banned.")
+
         else:
-            _, _, password, _ = secret()
+            _, _, password, _ = secret()  # Valid or not users access
             if command == password:
                 chat_id_verified["/USER" + str(len(chat_id_verified) + 1)] = chat_id
                 inverse_chat_id_verified.update({v: k for k, v in chat_id_verified.items()})
